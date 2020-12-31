20813c41e4d59d01480aba912fa54c025d2d8923
mohsin0176 <mohsin2010ju@gmail.com>
2020-12-31 21:08:20 +0600

    Add file type

--- Tutorial/File/File5.c
+++ Tutorial/File/File5.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    FILE *fptr;
+    char str[100];
+    fptr=fopen("print.txt","w");
+    printf("Enter String:\n");
+    while(strlen(gets(str)>0)
+          {
+              fputs(str,fptr);
+              puts("\n\n");
+          }
+    fclose(fptr);
+    fptr=fopen("print.txt","r");
+    while(fgets(str,100,fptr)!=NULL)
+        {
+            puts(str);
+            puts("\n\n");
+        }
+        fclose(fptr);
+    return 0;
+}
