eaa97a5bbc7aef097d572c0cc016d2241f0c05fb
mohsin0176 <mohsin2010ju@gmail.com>
2020-12-31 21:22:03 +0600

    Add file type

--- Programs/FP/openfile.c
+++ Programs/FP/openfile.c
@@ -1,1 +1,21 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char ch,file_name[25];
+    FILE *fp;
+    printf("Enter The Name Of File You Wish To See:\n");
+    gets(file_name);
+    fp=fopen(file_name,"r");
+    if(fp==NULL)
+    {
+        perror("Error while Opening The File.\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("The contents of %s file are:\n",file_name);
+    while((ch=fgetc(fp))!=EOF)
+        printf("%c",ch);
+    fclose(fp);
+    return 0;
+}
