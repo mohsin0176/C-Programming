436dd26bd599b89ad4808cd0f0e2ea2f6db9600b
mohsin0176 <mohsin2010ju@gmail.com>
2020-12-31 21:28:09 +0600

    Add file type

--- Programs/FP/deletfile.c
+++ Programs/FP/deletfile.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int status;
+    char file_name[25];
+    printf("Enter The Name of The file You Wish to delete:\n");
+    gets(file_name);
+    status=remove(file_name);
+    if(status==0)
+    {
+        printf("%s File Deleted Success.",file_name);
+    }
+    else
+    {
+    printf("Unable to Delete File.\n");
+    perror("Error");
+    }
 
+    return 0;
+}
