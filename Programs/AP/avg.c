abc7cba26fe19015fb4d81deeb8865ec149e1ee1
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-01 21:58:24 +0600

    Add file type

--- Programs/AP/avg.c
+++ Programs/AP/avg.c
@@ -1,1 +1,16 @@
 
+#include<stdio.h>
+int main()
+
+{
+    int i,sum=0,num[10];
+    for(i=0;i<=9;i++)
+    {
+        scanf("%d",&num[i]);
+        sum=sum+num[i];
+    }
+
+
+    printf("Average is:%d",sum/9);
+    return 0;
+}
