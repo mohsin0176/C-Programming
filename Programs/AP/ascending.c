5bfd6e0a710e50571ac7c15783ce2af5faf182b2
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-01 22:13:20 +0600

    Add file type

--- Programs/AP/ascending.c
+++ Programs/AP/ascending.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
 
+int main()
+{
+    int s,temp,i,j,a[20];
+    scanf("%d",&s);
+    for(i=0;i<=s;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=s-2;i>=0;i--)
+    {
+        for(j=0;j<=i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+
+    for(i=0;i<s;i++)
+    {
+        printf("%d",a[i]);
+    }
+    return 0;
+}
--- Programs/AP/binary.c
+++ Programs/AP/binary.c
@@ -1,1 +1,43 @@
+#include<stdio.h>
+int main()
+{
 
+    int a[10],i,n,m,c=0,l,u,mid;
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&m);
+    l=0;
+    u=n-1;
+    while(l<=u)
+    {
+        mid=(l+u)/2;
+        if(m==a[mid])
+        {
+            c=1;
+            break;
+        }
+        else if(m<a[mid])
+        {
+            u=mid-1;
+        }
+        else
+        {
+
+
+            l=mid+1;
+        }
+    }
+    if(c==0)
+    {
+        printf("Number not found");
+    }
+    else
+    {
+        printf("Number Found");
+    }
+
+    return 0;
+}
